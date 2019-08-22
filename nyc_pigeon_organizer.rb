8c9b20c94084c57dea83fc6396e5680a077c3e0b
Andrew Thiessen <squaids449@gmail.com>
2019-08-22 08:00:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,9 @@ def nyc_pigeon_organizer(data)
          list[name] ||= {}
          list[name][key] ||= []
          list[name][key] << inner_v.to_s
-         # ||= is very weird but I was browsing forums and found that it may work for this case
+         #I couldnt figure out how to put more than where the pigeon lived at first 
+         #but then I found the "OR equals" ||= and saw it has a lot of interesting uses
+         # ||= is very bizzare but I was browsing forums and found that it may work for this case
          #when I saw all the tests pass and caused me to search further
          #almost as if it iterates WITHIN the iteration WITHIN a hash thats WITHIN another hash...
          #sheesh
