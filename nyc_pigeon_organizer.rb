9c394a9f64f4d40e9698c1ac4f3bf1471cf60300
Andrew Thiessen <squaids449@gmail.com>
2019-08-22 07:43:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), list|
     value.each do |inner_v, names|
       names.each do |name|
-         list[name] = {}
-         list[name][key] = []
+         list[name] ||= {}
+         list[name][key] ||= []
          
       end
     end
