14ba7a5800536a386557ff08d3bb1f78265447fa
Andrew Thiessen <squaids449@gmail.com>
2019-08-22 07:57:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,11 +3,13 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), list|
     value.each do |inner_v, names|
       names.each do |name|
-         list[name] = {}
+         list[name] ||= {}
          list[name][key] ||= []
          list[name][key] << inner_v.to_s
          # ||= is very weird but I was browsing forums and found that it may work for this case
          #when I saw all the tests pass and caused me to search further
+         #almost as if it iterates WITHIN the iteration WITHIN a hash thats WITHIN another hash...
+         #sheesh
          
       end
     end
